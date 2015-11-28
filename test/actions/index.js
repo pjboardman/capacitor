6f144daa0f5676a69e15faf311a7b4969de8fed9
Phil Boardman <pboardman@gmail.com>
2015-11-27 23:05:39 -0600

    adding additional specs

--- test/actions/index.js
+++ test/actions/index.js
@@ -4,19 +4,24 @@ describe('actions', () => {
   describe('creating an action', () => {})
 
   describe('invoking an action', () => {
+
     it('should return a promise')
   })
 
-  describe('receiving an action dispatch', () => {
-    it('should receive the mutation if registered')
-    it('should not receive the mutations if unregistered')
-    it('should not receive the mutation if a previous dispatch failed')
+  describe('dispatching an action', () => {
+
+    it('registered listener should receive the mutation')
+    it('unregistered listener should not receive the mutations')
+    it('should stop dispatching on the first error')
+    it('should handle a listener not returning a promise')
   })
+
   describe('watching an action', () => {
 
     it('should allow watching all events')
 
     describe('should allow watching specific events', () => {
+
       it('started')
       it('finished')
       it('error')
---
+++ test/actions/registry.js
@@ -0,0 +1,10 @@
+
+describe('action registry', () => {
+  it('should accept a collection of actions on construction')
+  it('should allow actions to be added')
+  it('should expose registered actions as properties')
+  describe('sending by type', () => {
+    it('should allow sending an action by type name')
+    it('should error if the type name does not exist')
+  })
+})
